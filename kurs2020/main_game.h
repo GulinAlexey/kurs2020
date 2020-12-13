997db8d87a3ee4cdc903411086db665a56954441
GulinAlexey <gulin-2001@mail.ru>
2020-12-13 15:38:00 +0700

    В окно игры добавлены надписи (label) о текущем количестве ресурсов

--- kurs2020/main_game.h
+++ kurs2020/main_game.h
@@ -39,6 +39,14 @@ namespace kurs2020 {
 	private: System::Windows::Forms::ToolStripMenuItem^  save_game_butt;
 	private: System::Windows::Forms::ToolStripMenuItem^  spravk_lent_butt;
 	private: System::Windows::Forms::ToolStripMenuItem^  go_to_main_menu_butt;
+	private: System::Windows::Forms::Label^  skorost_zhiz_text;
+	private: System::Windows::Forms::Label^  den_procv_text;
+	private: System::Windows::Forms::Label^  kolvo_krest_text;
+	private: System::Windows::Forms::Label^  budget_text;
+	private: System::Windows::Forms::Label^  hleb_text;
+	private: System::Windows::Forms::Label^  skot_text;
+
+
 
 	private:
 		/// <summary>
@@ -58,6 +66,12 @@ namespace kurs2020 {
 			this->save_game_butt = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->spravk_lent_butt = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->go_to_main_menu_butt = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->skorost_zhiz_text = (gcnew System::Windows::Forms::Label());
+			this->den_procv_text = (gcnew System::Windows::Forms::Label());
+			this->kolvo_krest_text = (gcnew System::Windows::Forms::Label());
+			this->budget_text = (gcnew System::Windows::Forms::Label());
+			this->hleb_text = (gcnew System::Windows::Forms::Label());
+			this->skot_text = (gcnew System::Windows::Forms::Label());
 			this->lent_menu->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -96,12 +110,91 @@ namespace kurs2020 {
 			this->go_to_main_menu_butt->Text = L"Выход в главное меню";
 			this->go_to_main_menu_butt->Click += gcnew System::EventHandler(this, &main_game::go_to_main_menu_butt_Click);
 			// 
+			// skorost_zhiz_text
+			// 
+			this->skorost_zhiz_text->AutoSize = true;
+			this->skorost_zhiz_text->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->skorost_zhiz_text->Font = (gcnew System::Drawing::Font(L"Georgia", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->skorost_zhiz_text->Location = System::Drawing::Point(12, 50);
+			this->skorost_zhiz_text->Name = L"skorost_zhiz_text";
+			this->skorost_zhiz_text->Size = System::Drawing::Size(212, 31);
+			this->skorost_zhiz_text->TabIndex = 1;
+			this->skorost_zhiz_text->Text = L"Скорость жизни: ";
+			// 
+			// den_procv_text
+			// 
+			this->den_procv_text->AutoSize = true;
+			this->den_procv_text->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->den_procv_text->Font = (gcnew System::Drawing::Font(L"Georgia", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->den_procv_text->Location = System::Drawing::Point(12, 91);
+			this->den_procv_text->Name = L"den_procv_text";
+			this->den_procv_text->Size = System::Drawing::Size(227, 31);
+			this->den_procv_text->TabIndex = 2;
+			this->den_procv_text->Text = L"Дни процветания: ";
+			// 
+			// kolvo_krest_text
+			// 
+			this->kolvo_krest_text->AutoSize = true;
+			this->kolvo_krest_text->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->kolvo_krest_text->Font = (gcnew System::Drawing::Font(L"Georgia", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->kolvo_krest_text->Location = System::Drawing::Point(12, 134);
+			this->kolvo_krest_text->Name = L"kolvo_krest_text";
+			this->kolvo_krest_text->Size = System::Drawing::Size(146, 31);
+			this->kolvo_krest_text->TabIndex = 3;
+			this->kolvo_krest_text->Text = L"Крестьяне: ";
+			this->kolvo_krest_text->Click += gcnew System::EventHandler(this, &main_game::label1_Click);
+			// 
+			// budget_text
+			// 
+			this->budget_text->AutoSize = true;
+			this->budget_text->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->budget_text->Font = (gcnew System::Drawing::Font(L"Georgia", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->budget_text->Location = System::Drawing::Point(12, 178);
+			this->budget_text->Name = L"budget_text";
+			this->budget_text->Size = System::Drawing::Size(164, 31);
+			this->budget_text->TabIndex = 4;
+			this->budget_text->Text = L"Бюджет: руб.";
+			// 
+			// hleb_text
+			// 
+			this->hleb_text->AutoSize = true;
+			this->hleb_text->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->hleb_text->Font = (gcnew System::Drawing::Font(L"Georgia", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->hleb_text->Location = System::Drawing::Point(12, 220);
+			this->hleb_text->Name = L"hleb_text";
+			this->hleb_text->Size = System::Drawing::Size(85, 31);
+			this->hleb_text->TabIndex = 5;
+			this->hleb_text->Text = L"Хлеб: ";
+			// 
+			// skot_text
+			// 
+			this->skot_text->AutoSize = true;
+			this->skot_text->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->skot_text->Font = (gcnew System::Drawing::Font(L"Georgia", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(204)));
+			this->skot_text->Location = System::Drawing::Point(12, 262);
+			this->skot_text->Name = L"skot_text";
+			this->skot_text->Size = System::Drawing::Size(208, 31);
+			this->skot_text->TabIndex = 6;
+			this->skot_text->Text = L"Домашний скот: ";
+			// 
 			// main_game
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(844, 501);
+			this->Controls->Add(this->skot_text);
+			this->Controls->Add(this->hleb_text);
+			this->Controls->Add(this->budget_text);
+			this->Controls->Add(this->kolvo_krest_text);
+			this->Controls->Add(this->den_procv_text);
+			this->Controls->Add(this->skorost_zhiz_text);
 			this->Controls->Add(this->lent_menu);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
 			this->MainMenuStrip = this->lent_menu;
@@ -120,5 +213,7 @@ namespace kurs2020 {
 	private: System::Void go_to_main_menu_butt_Click(System::Object^  sender, System::EventArgs^  e) {
 				 Close();
 			 }
-	};
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 }
+};
 }
