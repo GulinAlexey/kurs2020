19f71a39d9d81d8ee88285448384bc5461068625
GulinAlexey <gulin-2001@mail.ru>
2021-01-09 22:50:42 +0700

    Исправлены столбцы таблицы. Добавлена кнопка для закрытия окна рекордов

--- kurs2020/record_tabl.h
+++ kurs2020/record_tabl.h
@@ -45,14 +45,19 @@ namespace kurs2020 {
 
 
 	private: System::Windows::Forms::DataGridView^  spisok;
-
-
-
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  number_tabl;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  name_tabl;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^  proizv_tabl;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  prozhorl_tabl;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  rashod_tabl;
+	private: System::Windows::Forms::Button^  cloz;
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -85,11 +90,9 @@ namespace kurs2020 {
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(record_tabl::typeid));
 			this->spisok = (gcnew System::Windows::Forms::DataGridView());
-			this->number_tabl = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->name_tabl = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->proizv_tabl = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->prozhorl_tabl = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->rashod_tabl = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->cloz = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->spisok))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -103,10 +106,9 @@ namespace kurs2020 {
 			this->spisok->BackgroundColor = System::Drawing::Color::Maroon;
 			this->spisok->ClipboardCopyMode = System::Windows::Forms::DataGridViewClipboardCopyMode::Disable;
 			this->spisok->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->spisok->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(5) {this->number_tabl, this->name_tabl, 
-				this->proizv_tabl, this->prozhorl_tabl, this->rashod_tabl});
+			this->spisok->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(2) {this->name_tabl, this->proizv_tabl});
 			this->spisok->EditMode = System::Windows::Forms::DataGridViewEditMode::EditProgrammatically;
-			this->spisok->Location = System::Drawing::Point(12, 12);
+			this->spisok->Location = System::Drawing::Point(78, 12);
 			this->spisok->MultiSelect = false;
 			this->spisok->Name = L"spisok";
 			this->spisok->ReadOnly = true;
@@ -115,70 +117,59 @@ namespace kurs2020 {
 			this->spisok->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
 			this->spisok->SelectionMode = System::Windows::Forms::DataGridViewSelectionMode::CellSelect;
 			this->spisok->ShowEditingIcon = false;
-			this->spisok->Size = System::Drawing::Size(850, 467);
+			this->spisok->Size = System::Drawing::Size(503, 467);
 			this->spisok->TabIndex = 14;
 			this->spisok->TabStop = false;
-			// 
-			// number_tabl
-			// 
-			this->number_tabl->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::None;
-			this->number_tabl->Frozen = true;
-			this->number_tabl->HeaderText = L"№";
-			this->number_tabl->Name = L"number_tabl";
-			this->number_tabl->ReadOnly = true;
-			this->number_tabl->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::Programmatic;
-			this->number_tabl->Width = 50;
 			// 
 			// name_tabl
 			// 
 			this->name_tabl->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::None;
 			this->name_tabl->Frozen = true;
-			this->name_tabl->HeaderText = L"Имя и фамилия";
+			this->name_tabl->HeaderText = L"Имя игрока";
+			this->name_tabl->MaxInputLength = 200;
 			this->name_tabl->Name = L"name_tabl";
 			this->name_tabl->ReadOnly = true;
 			this->name_tabl->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::Programmatic;
-			this->name_tabl->Width = 195;
+			this->name_tabl->Width = 170;
 			// 
 			// proizv_tabl
 			// 
 			this->proizv_tabl->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::None;
 			this->proizv_tabl->Frozen = true;
-			this->proizv_tabl->HeaderText = L"Производительность за один Час Процветания (хлеб/скот)";
+			this->proizv_tabl->HeaderText = L"Значение рекорда (кол-во набранных Дней Процветания)";
+			this->proizv_tabl->MaxInputLength = 200;
 			this->proizv_tabl->Name = L"proizv_tabl";
 			this->proizv_tabl->ReadOnly = true;
 			this->proizv_tabl->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::Programmatic;
-			this->proizv_tabl->Width = 200;
+			this->proizv_tabl->Width = 330;
 			// 
-			// prozhorl_tabl
+			// cloz
 			// 
-			this->prozhorl_tabl->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::None;
-			this->prozhorl_tabl->Frozen = true;
-			this->prozhorl_tabl->HeaderText = L"Прожорливость за один Час Процветания (хлеб/скот)";
-			this->prozhorl_tabl->Name = L"prozhorl_tabl";
-			this->prozhorl_tabl->ReadOnly = true;
-			this->prozhorl_tabl->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::Programmatic;
-			this->prozhorl_tabl->Width = 200;
-			// 
-			// rashod_tabl
-			// 
-			this->rashod_tabl->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::None;
-			this->rashod_tabl->Frozen = true;
-			this->rashod_tabl->HeaderText = L"Денежные расходы за один Час Процветания (руб.)";
-			this->rashod_tabl->Name = L"rashod_tabl";
-			this->rashod_tabl->ReadOnly = true;
-			this->rashod_tabl->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::Programmatic;
-			this->rashod_tabl->Width = 200;
+			this->cloz->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->cloz->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->cloz->DialogResult = System::Windows::Forms::DialogResult::Cancel;
+			this->cloz->Location = System::Drawing::Point(587, 12);
+			this->cloz->Name = L"cloz";
+			this->cloz->Size = System::Drawing::Size(68, 38);
+			this->cloz->TabIndex = 16;
+			this->cloz->TabStop = false;
+			this->cloz->Text = L"Закрыть";
+			this->cloz->UseVisualStyleBackColor = true;
+			this->cloz->Click += gcnew System::EventHandler(this, &record_tabl::cloz_Click);
 			// 
 			// record_tabl
 			// 
+			this->AcceptButton = this->cloz;
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"$this.BackgroundImage")));
-			this->ClientSize = System::Drawing::Size(874, 491);
+			this->CancelButton = this->cloz;
+			this->ClientSize = System::Drawing::Size(658, 491);
+			this->Controls->Add(this->cloz);
 			this->Controls->Add(this->spisok);
 			this->DoubleBuffered = true;
 			this->MinimizeBox = false;
-			this->MinimumSize = System::Drawing::Size(890, 530);
+			this->MinimumSize = System::Drawing::Size(543, 530);
 			this->Name = L"record_tabl";
 			this->ShowIcon = false;
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
@@ -191,26 +182,6 @@ namespace kurs2020 {
 		}
 #pragma endregion
 	private: System::Void record_tabl_Activated(System::Object^  sender, System::EventArgs^  e) {
-		if(f_endgame==1) //игра закончилась, закрыть все формы
-				 Close();
-		else
-		{
-			//очистить все строки перед выводом
-			while (0 != spisok->RowCount)
-				spisok->Rows->RemoveAt(0);
-
-			int kolvo_str=derevn.get_kolvo_krest(); //получить кол-во крестьян в списке
-			for(int num=0; num<kolvo_str; num++)
-			{
-				spisok->Rows->Add(); //добавить новую строку в таблицу
-				spisok->Rows[num]->Cells[0]->Value = Convert::ToString(num+1); //записать порядковый номер крестьянина
-				string str_name= derevn.krests[num].get_name() + " " + derevn.krests[num].get_surname();
-				spisok->Rows[num]->Cells[1]->Value = gcnew System::String(str_name.c_str()); //внести в ячейку таблицы
-				spisok->Rows[num]->Cells[2]->Value = Convert::ToString(derevn.krests[num].get_proizv_hleb()) + L"/" + Convert::ToString(derevn.krests[num].get_proizv_skot());
-				spisok->Rows[num]->Cells[3]->Value = Convert::ToString(derevn.krests[num].get_eda_hleb()*derevn.get_speed_life()) + L"/" + Convert::ToString(derevn.krests[num].get_eda_skot()*derevn.get_speed_life());
-				spisok->Rows[num]->Cells[4]->Value = Convert::ToString(derevn.krests[num].get_money_trat()*derevn.get_speed_life());
-			}
-		}
 
 		}
 
@@ -223,6 +194,9 @@ private: System::Void record_tabl_Load(System::Object^  sender, System::EventArg
 private: System::Void trud_butt_Click(System::Object^  sender, System::EventArgs^  e) {
 		 }
 private: System::Void timer_proverk_krest_Tick(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cloz_Click(System::Object^  sender, System::EventArgs^  e) {
+			 Close();
 		 }
 };
 }
